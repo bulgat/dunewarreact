3a7e1cfad309710d9a5b86523cc697ad4e53f063
kol <sirbulgat@gmail.com>
2022-11-18 17:52:33 +0300

    add carActive

--- src/reducer/rootReducer.js
+++ src/reducer/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import CarReducers from './car.js'
+import carActive from "./carActive.js";
 
 export const rootReducer = combineReducers({
-    cars:CarReducers
+    cars:CarReducers,
+    active: carActive
 })
 
 export default rootReducer;
