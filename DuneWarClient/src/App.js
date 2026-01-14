616e55a2430ced468a178fbf7c322ff57925fc19
Nikolay K <sirbulgat@gmail.com>
2026-01-14 21:55:33 +0300

    add footer css bottom down

--- DuneWarClient/src/App.js
+++ DuneWarClient/src/App.js
@@ -3,62 +3,60 @@ import './App.css';
 import "./duneHide.js";
 import "./globalDune.js";
 import React from "react";
-import {Routes,Route} from 'react-router-dom';
-import {HomePage} from './pages/Homepage';
-import {AboutPage} from './pages/AboutPage';
-import {BlogPage} from './pages/Blogpage';
-import {SinglePage} from './pages/SinglePage';
-import {UnitPage} from './pages/UnitPage';
-import {NotFoundPage} from './pages/NotFoundPage';
-import {Editpage} from './pages/Editpage';
-import {NavBarDune} from './components/NavBarDune'
-import {Loginpage} from './pages/Loginpage';
-import {CommentPage} from './pages/CommentPage';
+import { Routes, Route } from 'react-router-dom';
+import { HomePage } from './pages/Homepage';
+import { AboutPage } from './pages/AboutPage';
+import { BlogPage } from './pages/Blogpage';
+import { SinglePage } from './pages/SinglePage';
+import { UnitPage } from './pages/UnitPage';
+import { NotFoundPage } from './pages/NotFoundPage';
+import { Editpage } from './pages/Editpage';
+import { NavBarDune } from './components/NavBarDune'
+import { Loginpage } from './pages/Loginpage';
+import { CommentPage } from './pages/CommentPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {RequireAuth} from './hoc/RequireAuth';
-import {Createpost } from './pages/Createpost'
-import {AuthProvider} from './hoc/Authprovider';
-import {TownPage} from './pages/TownPage';
-//refux
-import {createStore} from 'redux';
+import { RequireAuth } from './hoc/RequireAuth';
+import { Createpost } from './pages/Createpost'
+import { AuthProvider } from './hoc/Authprovider';
+import { TownPage } from './pages/TownPage';
+import { createStore } from 'redux';
 import rooReducer from './reducer/rootReducer';
-import {Provider} from 'react-redux'
-
+import { Provider } from 'react-redux';
+import { FooterComponent } from './components/footer.component';
 function App() {
-	
-  const store = createStore(rooReducer);
 
-  return (
-    <Provider store ={store}>
-<NavBarDune/>
-<AuthProvider>
-  <Routes>
-      <Route path='/' element ={<HomePage/>}/>
-      <Route path='about/*' element ={<AboutPage/>}>
-        <Route path="contacts" element ={<p>contact</p>}/>
-        <Route path="team" element ={<><h2>super</h2><p>team</p></>}/>
-      </Route>
-      <Route path='/unitpage' element ={<UnitPage/>}/>
-      <Route path='/post' element ={<BlogPage/>}/>
-      <Route path='/post/:id' element ={<SinglePage/>}/>
-      <Route path='/post/:id/edit' element ={<Editpage/>}/>
-      
-      <Route path='*' element ={<NotFoundPage/>}/>
-      <Route path='post/new' element={<RequireAuth>
-        <Createpost/>
-      </RequireAuth>}
-      />
-      <Route path='/login' element ={<Loginpage/>}/>
-      <Route path='/town/:id' element ={<TownPage/>}/>
-      <Route path='/commentpage' element ={<CommentPage/>}/>
-    
-  </Routes>
-</AuthProvider>
-      <header>
-      
-      </header>
-    </Provider>
-  );
+    const store = createStore(rooReducer);
+
+    return (
+        <Provider store={store}>
+            <NavBarDune />
+            <AuthProvider>
+                <Routes>
+                    <Route path='/' element={<HomePage />} />
+                    <Route path='about/*' element={<AboutPage />}>
+                        <Route path="contacts" element={<p>contact</p>} />
+                        <Route path="team" element={<><h2>super</h2><p>team</p></>} />
+                    </Route>
+                    <Route path='/unitpage' element={<UnitPage />} />
+                    <Route path='/post' element={<BlogPage />} />
+                    <Route path='/post/:id' element={<SinglePage />} />
+                    <Route path='/post/:id/edit' element={<Editpage />} />
+
+                    <Route path='*' element={<NotFoundPage />} />
+                    <Route path='post/new' element={<RequireAuth>
+                        <Createpost />
+                    </RequireAuth>}
+                    />
+                    <Route path='/login' element={<Loginpage />} />
+                    <Route path='/town/:id' element={<TownPage />} />
+                    <Route path='/commentpage' element={<CommentPage />} />
+
+                </Routes>
+            </AuthProvider>
+   
+            <FooterComponent/>
+        </Provider>
+    );
 }
 
 export default App;
---
+++ DuneWarClient/src/components/footer.component.jsx
@@ -0,0 +1,10 @@
+import './footer.component.css'
+
+const FooterComponent =()=>{
+    return(
+        <>
+            <div className="footer-bottom">dune war strategy</div>
+        </>
+    )
+}
+export { FooterComponent }
