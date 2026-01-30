f71e97e74bf1a58d641c3078ebcc3cad4edb0a99
Nikolay K <sirbulgat@gmail.com>
2026-01-30 23:20:47 +0300

    add class+class FAT dinamic!

--- DuneWarClient/src/components/infoLine.component.jsx
+++ DuneWarClient/src/components/infoLine.component.jsx
@@ -1,13 +1,21 @@
 
 import { useEffect, useState } from 'react'
+import './infoLine.component.css'
 
 const InfoLineComponent = ({ item, index, Label }) => {
-
+    const style = true;
 
 
     return (
 
-        <li key={item.id}>{index}) { Label } id:{item.id}   {item.name} **** ****{item.numCannon} <a href={'arsenal/' + item.id}>Текст ссылки</a> </li>
+        <li key={item.id} className='li-info'>
+            <div className='line-info'>
+                <div className='mini-column'>{index})</div>
+                <div className={`mini-column ${style ? 'fat' : ''}`} >{Label}</div>
+                <div className='mini-column'>id:{item.id}</div>
+                     {item.name} **** ****{item.numCannon} </div>
+            <a href={'arsenal/' + item.id}>Текст ссылки</a>
+        </li>
  
     )
 }
