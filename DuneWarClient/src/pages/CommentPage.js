e7e934fbd1f9eefab6c4b28a043c923779fe4635
Nikolay K <sirbulgat@gmail.com>
2026-02-10 22:33:49 +0300

    add style commentPage fat-width select menu

--- DuneWarClient/src/pages/CommentPage.js
+++ DuneWarClient/src/pages/CommentPage.js
@@ -110,6 +110,19 @@ const CommentPage = () => {
                         onChange={handleChange}
                         options={options}
                     />
+                    <br />
+                    <br />
+                    <Select
+                        defaultValue="lucy"
+                        style={{ width: 120 }}
+                        onChange={handleChange}
+                        options={[
+                            { value: 'jack', label: 'Jack' },
+                            { value: 'lucy', label: 'Lucy' },
+                            { value: 'Yiminghe', label: 'yiminghe' },
+                            { value: 'disabled', label: 'Disabled', disabled: true },
+                        ]}
+                    />
 
                     <br />
                     <br />
